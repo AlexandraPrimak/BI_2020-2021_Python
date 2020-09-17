7b3ed74e53751529e79ac6ea1f546425fca26039
AlexandraPrimak <primak.msu@mail.ru>
2020-09-17 20:44:25 +0300

    Corrected after Lint

--- converter.py
+++ converter.py
@@ -1,15 +1,21 @@
 # Конвертер, переводящий все величины в литры (дм^3)
-value = int(input()) # предлагаем ввести число, которое будем переводить в литры
-dimension = input() + '^3' # предлагаем ввести единицы измерения командой input и заносим это в переменную dimension
-if dimension == 'sm^3': # в ifах перечисляем возможные варианты переменной dimension и говорим, какую команду (print(арифметическое действие)) для каждого случая надо выполнить
-    print(value * (10**(-3)))
-elif dimension == 'mm^3': 
-    print(value * (10** (-6)))
-elif dimension == 'dm^3': 
+# предлагаем ввести число, которое будем переводить в литры
+value = int(input()) 
+# предлагаем ввести единицы измерения командой input и заносим это в dimension
+dimension = input() + '^3'  
+# в ifах перечисляем возможные варианты переменной dimension и говорим, 
+# какую команду (print(арифметич. действие)) для каждого случая надо выполнить
+if dimension == 'sm^3':
+    print(value * (10 ** (-3)))
+elif dimension == 'mm^3':
+    print(value * (10 ** (-6)))
+elif dimension == 'dm^3':
     print(value * 1)
-elif dimension == 'm^3': 
+elif dimension == 'm^3':
     print(value * 1000)
-elif dimension == 'km^3': 
+elif dimension == 'km^3':
     print(value * (10000 ** 3))
-else: #если ни одно из вышеперечисленных условий не выполнено, конвертер выведет 'Cannot perform operation'
+# если ни одно из вышеперечисленных условий не выполнено, 
+# конвертер выведет 'Cannot perform operation'
+else:
     print('Cannot perform operation')
